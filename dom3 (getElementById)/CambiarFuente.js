451b263e30ee97eaf4093119192953eb716b828c
LautaroCid <162642931+LautaroCid@users.noreply.github.com>
2024-04-05 19:50:28 -0300

    Add files via upload

---
+++ dom3 (getElementById)/CambiarFuente.js
@@ -0,0 +1,12 @@
+function cambiarFuente10() {
+    let tit = document.getElementById('comentarios')
+    tit.style.fontSize = '10px'
+}
+function cambiarFuente13() {
+    let tit = document.getElementById('comentarios')
+    tit.style.fontSize = '13px'
+}
+function cambiarFuente20() {
+    let tit = document.getElementById('comentarios')
+    tit.style.fontSize = '20px'
+}
